ac45d341eebb363e012e023fb9ca73af513f86fb
Ansari839 <abdullahansari839@gmail.com>
2024-01-23 09:48:55 +0500

    Navbar Complete

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className='text-danger'>
+        Hello World
+      </h1>
+    </main>
+  )
+}
